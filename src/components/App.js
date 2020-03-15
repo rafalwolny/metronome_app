a39aa8936a91b6d8006cc2de3a6eb3b5967711a7
Rafal Wolny <rafal.wolny96@gmail.com>
2020-03-15 16:19:44 +0100

    Initial commit, webpack, babel, react installed

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import MainDisplay from './MainDisplay'
+
+class App extends Component{
+  render() {
+    return(
+      <div>
+        <MainDisplay />
+      </div>
+    );
+  }
+}
+export default App;
